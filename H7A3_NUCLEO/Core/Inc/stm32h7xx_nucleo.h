4a54f993c9c00b221755c0b475ade6c770efb6e1
Cullen Sharp <cullen.sharp100@gmail.com>
2025-11-02 12:03:51 -0800

    Configures spi and updates include paths

---
+++ H7A3_NUCLEO/Core/Inc/stm32h7xx_nucleo.h
@@ -0,0 +1,13 @@
+/*
+ * stm32h7xx_nucleo.h
+ *
+ *  Created on: Nov 2, 2025
+ *      Author: cullen-sharp
+ */
+
+#ifndef INC_STM32H7XX_NUCLEO_H_
+#define INC_STM32H7XX_NUCLEO_H_
+
+
+
+#endif /* INC_STM32H7XX_NUCLEO_H_ */
