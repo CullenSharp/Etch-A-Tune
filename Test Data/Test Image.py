6b5d3961bafac42b4d6d54079b46e4645f320f21
ConsenzPDX <consenz@pdx.edu>
2025-11-02 23:29:44 -0800

    Fixed the test data so it should be properly formatted now

--- Test Data/Test Image.py
+++ Test Data/Test Image.py
@@ -1,10 +1,7 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-import operator
-import matplotlib.image as mpimg
-import scipy as sp
+import cv2
 
 
 PI2 = np.pi*2
@@ -42,24 +39,36 @@ def main(signal, image):
     '''
 
     with open('test_vector.h', 'w') as out_file:
-        out_file.write('{0} test_vector[] = {')
-        for sample in signal:
-            out_file.write(f'{sample},')
+        out_file.write(f'float test_vector[{len(signal)}]')
+        out_file.write(' = {')
+        for i in range(1,len(signal)):
+            out_file.write(f'{signal[i]}')
+            '''Loop to append comma to element, and stops at last element to remove excess comma'''
+            if i != len(signal) - 1:
+                    out_file.write(',')
         out_file.write('};')
 
+    rows, columns = image.size
     with open('test_image.h', 'w') as out_image:
-        out_image.write('{0} test_image[] = {')
-        for sample in image:
-            out_image.write(f'{sample},')
+        out_image.write(f'float test_image[{rows}][{columns}]')
+        out_image.write(' = {')
+        for i in range(rows):
+            out_image.write('{')
+            for j in range(columns):
+                L,A = image.getpixel((i,j))
+                out_image.write('{L}'.format(L=L))
+                if j != columns - 1:
+                    out_image.write(',')
+            out_image.write('}')
+            if i != rows - 1:
+                out_image.write(',')
         out_image.write('};')
 
 ts = np.linspace(start, end, sample_rate)
 signal = linear_chirp(ts)
-plt.plot(ts, signal)
-plt.show()
 
 img = Image.open('Test Image A 480x320.png').convert('LA')
 img.save('grayscale.png')
-img = np.array(img)
+#img = cv2.imread('grayscale.png')
 
 main(signal, img)
