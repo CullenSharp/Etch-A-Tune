c098195a9c65da35612ebd3688bc5b09bb348fb9
Cullen Sharp <cullen.sharp100@gmail.com>
2025-11-09 19:30:17 -0800

    Moves st7796 function prototypes into st7796.h

--- SPI_screen/Core/Src/Lcd/st7796.c
+++ SPI_screen/Core/Src/Lcd/st7796.c
@@ -8,25 +8,6 @@
 #include <Lcd/st7796.h>
 #include "main.h"
 
-void     st7796_Init(void);
-uint32_t st7796_ReadID(void);
-void     st7796_DisplayOn(void);
-void     st7796_DisplayOff(void);
-void     st7796_SetCursor(uint16_t Xpos, uint16_t Ypos);
-void     st7796_WritePixel(uint16_t Xpos, uint16_t Ypos, uint16_t RGB_Code);
-uint16_t st7796_ReadPixel(uint16_t Xpos, uint16_t Ypos);
-void     st7796_SetDisplayWindow(uint16_t Xpos, uint16_t Ypos, uint16_t Width, uint16_t Height);
-void     st7796_DrawHLine(uint16_t RGBCode, uint16_t Xpos, uint16_t Ypos, uint16_t Length);
-void     st7796_DrawVLine(uint16_t RGBCode, uint16_t Xpos, uint16_t Ypos, uint16_t Length);
-void     st7796_FillRect(uint16_t Xpos, uint16_t Ypos, uint16_t Xsize, uint16_t Ysize, uint16_t RGBCode);
-uint16_t st7796_GetLcdPixelWidth(void);
-uint16_t st7796_GetLcdPixelHeight(void);
-void     st7796_DrawBitmap(uint16_t Xpos, uint16_t Ypos, uint8_t *pbmp);
-void     st7796_DrawRGBImage(uint16_t Xpos, uint16_t Ypos, uint16_t Xsize, uint16_t Ysize, uint16_t *pData);
-void     st7796_ReadRGBImage(uint16_t Xpos, uint16_t Ypos, uint16_t Xsize, uint16_t Ysize, uint16_t *pData);
-void     st7796_Scroll(int16_t Scroll, uint16_t TopFix, uint16_t BottonFix);
-void     st7796_UserCommand(uint16_t Command, uint8_t * pData, uint32_t Size, uint8_t Mode);
-
 LCD_DrvTypeDef   st7796_drv =
 {
   st7796_Init,
