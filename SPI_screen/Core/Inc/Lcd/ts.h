e1ba0575141a4e304c857a2873e603ceeba74d51
Cullen Sharp <cullen.sharp100@gmail.com>
2025-11-29 09:49:37 -0800

    Replace XPT2046 driver with simple analog touch driver

--- SPI_screen/Core/Inc/Lcd/ts.h
+++ SPI_screen/Core/Inc/Lcd/ts.h
@@ -1,107 +1,51 @@
-/**
-  ******************************************************************************
-  * @file    ts.h
-  * @author  MCD Application Team
-  * @version V4.0.1
-  * @date    21-July-2015
-  * @brief   This file contains all the functions prototypes for the Touch Screen driver.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2015 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+/*
+ * ts.h
+ *
+ *  Created on: Nov 28, 2025
+ *      Author: cullen-sharp
+ */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __TS_H
-#define __TS_H
+#ifndef __TS_H_
+#define __TS_H_
 
-#ifdef __cplusplus
- extern "C" {
-#endif
+#define N_SAMPLES 2
+
+/* Interface section */
 
 /* Includes ------------------------------------------------------------------*/
-#include <stdint.h> 
+#include <stdint.h>
+#include "stm32h7a3xxq.h"
+#include "stm32h7xx_ll_gpio.h"
 
-/** @addtogroup BSP
-  * @{
-  */
+typedef struct {
+    // State variables for x and y
+    uint32_t x;
+    uint32_t y;
+} TS_Point;
 
-/** @addtogroup Components
-  * @{
-  */
-    
-/** @addtogroup TS
-  * @{
-  */
+typedef struct {
+    /**
+     * @brief Construct a new TS_Drv object
+     *
+     * @param xp X+ pin. Must be an analog pin
+     * @param yp Y+ pin. Must be an analog pin
+     * @param xm X- pin. Can be a digital pin
+     * @param ym Y- pin. Can be a digital pin
+     */
+    void 	(*Init)(void);
 
-/** @defgroup TS_Exported_Types
-  * @{
-  */
+    // public methods
+    uint32_t 	(*Read_Touch_Y)();
+    uint32_t 	(*Read_Touch_X)();
+    TS_Point 	(*Get_Point)();
 
-/** @defgroup TS_Driver_structure  Touch Sensor Driver structure
-  * @{
-  */
-typedef struct
-{  
-  void       (*Init)(uint16_t);
-  uint16_t   (*ReadID)(uint16_t);
-  void       (*Reset)(uint16_t);
-  void       (*Start)(uint16_t);
-  uint8_t    (*DetectTouch)(uint16_t);
-  void       (*GetXY)(uint16_t, uint16_t*, uint16_t*);
-  void       (*EnableIT)(uint16_t);
-  void       (*ClearIT)(uint16_t);
-  uint8_t    (*GetITStatus)(uint16_t);
-  void       (*DisableIT)(uint16_t);
-}TS_DrvTypeDef;
-/**
-  * @}
-  */
+    // private variables
+    // pins
+    uint32_t  _xp_pin,  _yp_pin,  _xm_pin,  _ym_pin;
+    // ports
+    GPIO_TypeDef *_xp_port, *_yp_port, *_xm_port, *_ym_port;
+} TS_Drv;
 
-/**
-  * @}
-  */
 
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __TS_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+#endif /* INC_LCD_TS_H_ */
---
+++ SPI_screen/Core/Src/Lcd/ts.c
@@ -0,0 +1,114 @@
+/*
+ * ts.c
+ *
+ *  Created on: Nov 28, 2025
+ *      Author: cullen-sharp
+ *
+ *	todo: create a macro or ts_drv attribute for adc channel
+ *	todo: write adc reads to be non-blocking
+ */
+
+#include "Lcd/ts.h"
+#include "main.h"
+
+// private variables
+extern ADC_HandleTypeDef hadc1;
+
+// function prototypes
+void 	 TS_Drv_init(void);
+uint32_t TS_Drv_Read_Touch_Y(void);
+uint32_t TS_Drv_Read_Touch_X(void);
+TS_Point TS_Drv_Get_Point(void);
+
+TS_Drv ts_drv = {
+	TS_Drv_init,
+	TS_Drv_Read_Touch_Y,
+	TS_Drv_Read_Touch_X,
+	TS_Drv_Get_Point,
+	XP_Pin, 			// _xp
+	YP_Pin, 			// _yp
+	XM_Pin, 			// _xm
+	YM_Pin, 			// _ym
+	XP_GPIO_Port,		// _xp_port
+	YP_GPIO_Port,		// _yp_port
+	XM_GPIO_Port,		// _xm_port
+	YM_GPIO_Port		// _ym_port
+};
+
+
+void TS_Drv_init(void) {
+	// not implemented yet
+}
+
+uint32_t TS_Drv_Read_Touch_Y(void) {
+	uint32_t s0, s1;
+
+	// |              | X+   | X-         | Y+   | Y-            |
+	// | Y-Coordinate | Hi-Z | Hi-Z / ADC | Gnd  | Vcc           |
+	LL_GPIO_SetPinMode(ts_drv._xp_port, ts_drv._xp_pin, LL_GPIO_MODE_INPUT);
+	LL_GPIO_SetPinMode(ts_drv._xm_port, ts_drv._xm_pin, LL_GPIO_MODE_ANALOG);
+	LL_GPIO_SetPinMode(ts_drv._yp_port, ts_drv._yp_pin, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(ts_drv._ym_port, ts_drv._ym_pin, LL_GPIO_MODE_OUTPUT);
+
+	LL_GPIO_SetOutputPin(ts_drv._ym_port, ts_drv._ym_pin);
+	LL_GPIO_ResetOutputPin(ts_drv._yp_port, ts_drv._yp_pin);
+
+	// delay for voltages to settle
+	// 5600 iterations roughly translates to 20us
+	for (volatile uint32_t i = 0; i < 5600; ++i);
+
+	// only read channel 14
+	LL_ADC_SetChannelPreselection(ADC1, LL_ADC_CHANNEL_14);
+	LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_14);
+
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, 10);
+	s0 = HAL_ADC_GetValue(&hadc1);
+
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, 10);
+	s1 = HAL_ADC_GetValue(&hadc1);
+
+	return (1023 - ((s0 + s1) >> 1));
+}
+
+uint32_t TS_Drv_Read_Touch_X(void) {
+	uint32_t s0, s1;
+
+	//|              | X+   | X-         | Y+   	  | Y-      |
+	//| X-Coordinate | Gnd  | Vcc        | Hi-Z / ADC | Hi-Z    |
+	LL_GPIO_SetPinMode(ts_drv._xp_port, ts_drv._xp_pin, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(ts_drv._xm_port, ts_drv._xm_pin, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(ts_drv._yp_port, ts_drv._yp_pin, LL_GPIO_MODE_ANALOG);
+	LL_GPIO_SetPinMode(ts_drv._ym_port, ts_drv._ym_pin, LL_GPIO_MODE_INPUT);
+
+	LL_GPIO_SetOutputPin(ts_drv._xm_port, 	ts_drv._xm_pin);
+	LL_GPIO_ResetOutputPin(ts_drv._xp_port, ts_drv._xp_pin);
+
+	// delay for voltages to settle
+	// 5600 iterations roughly translates to 20us
+	for (volatile uint32_t i = 0; i < 5600; ++i);
+
+	// only read channel 17
+	LL_ADC_SetChannelPreselection(ADC1, LL_ADC_CHANNEL_17);
+	LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_17);
+
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, 10);
+	s0 = HAL_ADC_GetValue(&hadc1);
+
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, 10);
+	s1 = HAL_ADC_GetValue(&hadc1);
+
+	return (1023 - ((s0 + s1) >> 1));
+}
+
+TS_Point TS_Drv_Get_Point(void) {
+	TS_Point result = {0, 0};
+
+	result.x = ts_drv.Read_Touch_X();
+	result.y = ts_drv.Read_Touch_Y();
+
+	return result;
+}
