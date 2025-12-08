3476a0fa8599a737216c6188571d4e1d7979eeb9
Cullen Sharp <cullen.sharp100@gmail.com>
2025-12-07 17:35:23 -0800

    Integrates single interrupt based encoder

--- SPI_screen/Core/Inc/stm32h7xx_it.h
+++ SPI_screen/Core/Inc/stm32h7xx_it.h
@@ -52,6 +52,8 @@ void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
+void EXTI1_IRQHandler(void);
+void EXTI2_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
 void TIM8_BRK_TIM12_IRQHandler(void);
 void TIM8_UP_TIM13_IRQHandler(void);
